ea729752e2e49db8b910c86f4b0ee7aa2161e434
Mark Barrie <mark.a.barrie@gmail.com>
2024-05-16 20:17:59 -0400

    finished resume page

--- src/components/About Me.jsx
+++ src/components/About Me.jsx
@@ -6,7 +6,7 @@ export default function AboutMe() {
             <div className="row m-0 about-me vh-100" >
                 <img className="col-3" src={Avatar} alt="image of Mark" />
                 <div className='col about-me-title'>
-                    <h1 >About Me</h1>
+                    <h1 className='text-shadow display-1'>About Me</h1>
                 </div>
                 <p className='row m-0'>Hello! Welcome to my Portolio Page. I have worked in the education field for 16 years and the professional audio industry for 20 years. As a web developer, I have experience working with the MERN stack and Javascript in web development. Most of my experience is with React and Javascript. Currently, I am attending a coding bootcamp in UCF to refine my skills in Full Stack development.</p>
             </div>
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,25 +1,42 @@
 export default function Resume() {
     return (
-        <div className="vh-100 container resume">
-            <h1 className="row">Resume</h1>
+        <div className="container resume">
+            <h1 className="row text-shadow">Resume</h1>
             <a className="row" href="/Mark_Barrie_Resume.pdf" download="Mark_Barrie_Resume.pdf">
                 Download Resume
             </a>
             <section className="resume-webdev row">
-                <h2>Proficiencies</h2>
-                <h3 >Web Development</h3>
-                <ul className="resume-list">
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>JavaScript</li>
-                    <li>React</li>
-                    <li>Express</li>
-                    <li>Node.js</li>
-                    <li>SQL</li>
-                    <li>NoSQL</li>
-                    <li>MongoDB</li>
-                    <li>Mongoose</li>
-                </ul>
+                <section className="mb-3">
+                    <h2 className="text-shadow">Proficiencies</h2>
+                    <h3 className="text-shadow">Web Development</h3>
+                    <ul className="resume-list">
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                        <li>React</li>
+                        <li>Express</li>
+                        <li>Node.js</li>
+                        <li>SQL</li>
+                        <li>NoSQL</li>
+                        <li>MongoDB</li>
+                        <li>Mongoose</li>
+                    </ul>
+                </section>
+                <section className="mb-3">
+                    <h3 className="text-shadow">APIs</h3>
+                    <ul className="resume-list">
+                        <li>Server-Side</li>
+                        <li>Third-Party</li>
+                    </ul>
+                </section>
+                <section className="mb-3">
+                    <h3 className="text-shadow">Development Tools</h3>
+                    <ul className="resume-list">
+                        <li>Git</li>
+                        <li>Git Hub</li>
+                        <li>Visual Studio Code</li>
+                    </ul>
+                </section>
             </section>
         </div>
     );
